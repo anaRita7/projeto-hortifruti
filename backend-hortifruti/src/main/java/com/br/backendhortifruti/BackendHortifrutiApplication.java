c3ffec4e60d7efdaffdbfcc2fa2177b09f88b8c9
anaRita7 <ana.cruz@ee.ufcg.edu.br>
2021-11-04 18:28:37 -0300

    anar - Geracao do projeto com Spring Boot

---
+++ backend-hortifruti/src/main/java/com/br/backendhortifruti/BackendHortifrutiApplication.java
@@ -0,0 +1,13 @@
+package com.br.backendhortifruti;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendHortifrutiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendHortifrutiApplication.class, args);
+	}
+
+}
