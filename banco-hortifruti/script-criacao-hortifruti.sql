38ee3079bbfb1e4addd82a5a1631f583de5d5fde
amandagsa <amandagsal@gmail.com>
2021-11-08 09:37:24 -0300

    script - criacao de banco

---
+++ banco-hortifruti/script-criacao-hortifruti.sql
@@ -0,0 +1,64 @@
+CREATE DATABASE hortifruti;
+
+USE hortifruti;
+
+CREATE TABLE cliente(
+	id_cliente bigint AUTO_INCREMENT NOT NULL,
+	tipo char,
+	documento varchar(50),
+	nome varchar(50),
+	PRIMARY KEY (id_cliente)
+);
+
+CREATE TABLE pedido(
+	id_pedido bigint AUTO_INCREMENT NOT NULL, 
+	codigo bigint,
+	data date,
+	id_cliente bigint NOT NULL,
+	id_endereco bigint NOT NULL,
+	situacao boolean,
+	quantidade_total int,
+	valor_total float(5,2),
+	valor_final float(5,2),
+	desconto float (3,2),
+	PRIMARY KEY (id_pedido)
+);
+
+CREATE TABLE produto(
+	id_produto bigint AUTO_INCREMENT NOT NULL,
+	codigo bigint,
+	nome varchar(30),
+	descricao varchar(50),
+	unidade_medida varchar(5), 
+	valor_unitario float(5,2),
+	status boolean,
+	PRIMARY KEY (id_produto)
+);
+
+CREATE TABLE endereco(
+	id_endereco bigint AUTO_INCREMENT NOT NULL,
+	cep varchar(9),
+	cidade varchar(30),
+	bairro varchar(30),
+	estado varchar(30),
+	rua varchar(30),
+	complemento varchar (30),
+	numero int,
+	PRIMARY KEY (id_endereco)
+);
+
+CREATE TABLE item_do_pedido(
+	id_item_do_pedido bigint AUTO_INCREMENT NOT NULL,
+	id_pedido bigint NOT NULL,
+	id_produto bigint NOT NULL,
+	quantidade int,
+	valor_total float(8,2),
+	PRIMARY KEY (id_item_do_pedido)
+);
+
+ALTER TABLE pedido ADD CONSTRAINT fk_cliente_pedido FOREIGN KEY (id_cliente) REFERENCES cliente (id_cliente);
+ALTER TABLE pedido ADD CONSTRAINT fk_endereco_pedido FOREIGN KEY (id_endereco) REFERENCES endereco (id_endereco);
+
+ALTER TABLE item_do_pedido ADD CONSTRAINT fk_pedido_item_do_pedido FOREIGN KEY (id_pedido) REFERENCES pedido (id_pedido);
+ALTER TABLE item_do_pedido ADD CONSTRAINT fk_produto_item_do_pedido FOREIGN KEY (id_produto) REFERENCES produto (id_produto);
+
