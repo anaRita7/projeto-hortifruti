685c2e2783901dfc82522d3419a4dff7ba9a71f5
Samyra França <samy_franca@outlook.com>
2021-11-17 22:34:02 -0300

    estrutura inicial

--- frontend-hortifruti/src/app/app.module.ts
+++ frontend-hortifruti/src/app/app.module.ts
@@ -6,10 +6,20 @@ import { AppComponent } from './app.component';
 import { FormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http'
 import { ProdutoService } from 'src/app/services/produto.service';
+import { RegisterComponent } from './views/products/register/register.component';
+import { ConsultComponent } from './views/products/consult/consult.component';
+import { HomeComponent } from './views/home/home/home.component';
+import { HeaderComponent } from './views/home/header/header.component';
+import { SidebarComponent } from './views/home/sidebar/sidebar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    RegisterComponent,
+    ConsultComponent,
+    HomeComponent,
+    HeaderComponent,
+    SidebarComponent
   ],
   imports: [
     BrowserModule,
