2b25c6ccbc41f43074582d8a3df5db6ef9b19fe1
Palloma Ruysa <pallomaruysa@gmail.com>
2021-11-22 17:53:32 -0300

    removendo constructor da class Cliente-entity

--- frontend-hortifruti/src/app/model/Cliente.ts
+++ frontend-hortifruti/src/app/model/Cliente.ts
@@ -1,16 +1,7 @@
 export class Cliente{
-    id: number;
-	nome: String;
-	tipo: String;
-	documento: String;
-	telefone: String;    
-
-    constructor(id: number, nome: String, tipo: String,
-        documento: String, telefone: String){
-            this.id = id;
-            this.nome = nome;
-            this.tipo = tipo;
-            this.documento = documento;
-            this.telefone = telefone;
-    }
+    id?: number;
+	nome?: String;
+	tipo?: String;
+	documento?: String;
+	telefone?: String;    
 }
