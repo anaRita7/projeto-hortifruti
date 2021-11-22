35d8eaf5e6a4fc76b9a9f3fa3da2d0bac805aef9
Palloma Ruysa <pallomaruysa@gmail.com>
2021-11-22 13:24:52 -0300

    corrigindo erro de metodos

--- frontend-hortifruti/src/app/views/clients/register-cli/register-cli.component.ts
+++ frontend-hortifruti/src/app/views/clients/register-cli/register-cli.component.ts
@@ -1,10 +1,9 @@
+import { Cliente } from 'src/app/model/Cliente';
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup } from '@angular/forms';
 import { Router } from '@angular/router';
 import { ClienteService } from 'src/app/services/cliente.service';
 import { FormValidations } from 'src/app/shered/form-validations';
-
-import { Cliente } from './../../../model/Cliente';
 
 @Component({
   selector: 'app-register',
@@ -12,24 +11,23 @@ import { Cliente } from './../../../model/Cliente';
   styleUrls: ['./register-cli.component.css']
 })
 export class RegisterCliComponent implements OnInit {
-  form: FormGroup;
-  cliente: Cliente = new Cliente();
+  form: FormGroup | undefined;
   constructor(
+    public cliente: Cliente,
     private service: ClienteService, 
     private router: Router,
     private formBuilder: FormBuilder
     ) { }
 
   ngOnInit(): void {
-    this.form = this.formBuilder.group({
-      cpf: this.formBuilder.control({ value: null, disabled: false}, FormValidations.isValidCpf())
-    })
+    
   }
-  postClient(){
+
+  postCliente(){
     this.service.postCliente(this.cliente)
     .subscribe(data => {
       alert('Cadastrado com sucesso');
-      this.router.navigate(["clients-register"]);
+      this.router.navigate(["clients-consult"]);
     })
   }
 
