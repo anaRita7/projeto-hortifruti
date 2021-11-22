96c1c222d157ec89d9ef4a765215d666c6e1dae5
bruna <lbruna886@gmail.com>
2021-11-22 12:10:27 -0300

    teste unitario com Mockito - incluirCliente

---
+++ backend-hortifruti/src/test/java/com/br/backendhortifruti/controllerTest/ClienteControllerMockTest.java
@@ -0,0 +1,71 @@
+package com.br.backendhortifruti.controllerTest;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Mockito.when;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.MockitoAnnotations;
+import org.mockito.junit.MockitoJUnitRunner;
+
+import com.br.backendhortifruti.model.entity.Cliente;
+import com.br.backendhortifruti.model.repository.ClienteRepository;
+import com.br.backendhortifruti.model.service.impl.ClienteServiceImpl;
+
+@RunWith(MockitoJUnitRunner.class)
+public class ClienteControllerMockTest {
+	
+	@Mock
+	private ClienteRepository clienteRepository;
+	
+	@InjectMocks
+	private ClienteServiceImpl clienteServiceImpl;
+	
+	@BeforeEach
+	public void init() {
+		MockitoAnnotations.openMocks(this);
+	}
+	
+	@DisplayName("Register a new client")
+	@Test
+	public void incluirClienteMockTest() {
+		Cliente cliente = new Cliente();
+		
+		when(clienteRepository.save(Mockito.any(Cliente.class))).thenReturn(cliente);
+		
+		cliente.setNome("Inajara Pereira");
+		cliente.setTipo('F');
+		cliente.setDocumento("063.785.409-85");
+		cliente.setTelefone("99999999999");
+		
+		Cliente clienteResponse = clienteServiceImpl.incluirCliente(cliente);
+		assertThat(clienteResponse.getNome().equals("Inajara Perei"));
+		assertThat(clienteResponse.getTipo() == 'F');
+		assertThat(clienteResponse.getNome().equals("063.785.409-85"));
+		assertThat(clienteResponse.getNome().equals("99999999999"));
+	}
+	
+	/*@DisplayName("Update client")
+	@Test
+	public void alterarClienteMockTest() {
+		Cliente cliente = new Cliente();
+		
+		when(clienteRepository.save(Mockito.any(Cliente.class))).thenReturn(cliente);
+		
+		cliente.setNome("Bruna Lima");
+		cliente.setTipo('F');
+		cliente.setDocumento("072.097.771-13");
+		cliente.setTelefone("99999999999");
+		
+		Cliente clienteResponse = clienteServiceImpl.alterarCliente(cliente, 2);
+		assertThat(clienteResponse.getNome().equals("Bruna Lima"));
+		assertThat(clienteResponse.getTipo() == 'F');
+		assertThat(clienteResponse.getNome().equals("072.097.771-13"));
+		assertThat(clienteResponse.getNome().equals("99999999999"));
+	} */
+}
