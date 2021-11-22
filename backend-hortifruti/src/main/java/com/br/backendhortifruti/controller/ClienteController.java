15c06544d3bf70ebe019ad57f0578a7e255ad48e
amandagsa <amandagsal@gmail.com>
2021-11-22 15:21:15 -0300

    Refatora metodos removendo codigo redundante

--- backend-hortifruti/src/main/java/com/br/backendhortifruti/controller/ClienteController.java
+++ backend-hortifruti/src/main/java/com/br/backendhortifruti/controller/ClienteController.java
@@ -33,16 +33,16 @@ public class ClienteController {
 	public ResponseEntity<ClienteIdDTO> incluirCliente(@RequestBody Cliente cliente) {
 		Cliente clienteResponse = clienteService.incluirCliente(cliente);
 		if(clienteResponse == null) {
-			return new ResponseEntity<ClienteIdDTO>(ClienteIdDTO.converter(clienteResponse), HttpStatus.UNPROCESSABLE_ENTITY);
+			return new ResponseEntity<>(ClienteIdDTO.converter(clienteResponse), HttpStatus.UNPROCESSABLE_ENTITY);
 		}
-		return new ResponseEntity<ClienteIdDTO>(ClienteIdDTO.converter(clienteResponse), HttpStatus.CREATED);
+		return new ResponseEntity<>(ClienteIdDTO.converter(clienteResponse), HttpStatus.CREATED);
 	}
 
 	@PutMapping("/{id}")
 	public ResponseEntity<ClienteDTO> alterarCliente(@PathVariable Integer id, @RequestBody Cliente cliente) {
 		try {
 			Cliente clienteResponse = clienteService.alterarCliente(cliente, id);
-			return new ResponseEntity<ClienteDTO>(ClienteDTO.converter(clienteResponse), HttpStatus.OK);
+			return new ResponseEntity<>(ClienteDTO.converter(clienteResponse), HttpStatus.OK);
 		}catch (NullPointerException e){
 			throw new NullPointerException();
 		}
@@ -52,7 +52,7 @@ public class ClienteController {
 	public ResponseEntity<ClienteDTO> consultarCliente(@PathVariable Integer id) {
 		try{
 			Cliente clienteResponse = clienteService.consultarCliente(id);
-			return new ResponseEntity<ClienteDTO>(ClienteDTO.converter(clienteResponse), HttpStatus.OK);
+			return new ResponseEntity<>(ClienteDTO.converter(clienteResponse), HttpStatus.OK);
 		}catch (NullPointerException e){
 			throw new NullPointerException();
 		}
@@ -62,14 +62,14 @@ public class ClienteController {
 	public ResponseEntity<PageImpl<ClienteDTO>> consultarClientes(Pageable pageable) {
 		Page<Cliente> page = clienteService.consultarClientes(pageable);
 		PageImpl<ClienteDTO> pageDTO = new PageImpl<> (ClienteDTO.converterLista(page.getContent()), pageable, page.getTotalElements());
-		return new ResponseEntity<PageImpl<ClienteDTO>>(pageDTO, HttpStatus.OK);
+		return new ResponseEntity<>(pageDTO, HttpStatus.OK);
 	}
 
 	@GetMapping("/documento/{documento}")
 	public ResponseEntity<ClienteIdDTO> consultarClientePorDocumento(@PathVariable("documento") String documento) {
 		try{
             Cliente cliente = clienteService.consultarClientePorDocumento(documento);
-            return new ResponseEntity<ClienteIdDTO>(ClienteIdDTO.converter(cliente), HttpStatus.OK);}
+            return new ResponseEntity<>(ClienteIdDTO.converter(cliente), HttpStatus.OK);}
 		catch (NullPointerException e){
 			throw new NullPointerException();
 		}
@@ -78,7 +78,7 @@ public class ClienteController {
 	@DeleteMapping("/{id}")
 	public ResponseEntity<String> excluirCliente(@PathVariable Integer id) {
 		clienteService.excluirCliente(id);
-		return new ResponseEntity<String>("Cliente excluído com sucesso!", HttpStatus.OK);
+		return new ResponseEntity<>("Cliente excluído com sucesso!", HttpStatus.OK);
 	}
 
 }
