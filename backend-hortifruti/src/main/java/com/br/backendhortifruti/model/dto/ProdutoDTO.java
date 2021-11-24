7ea244f7bf352bcf2f2b2b78e92fee5ebd5d18f5
anaRita7 <ana.cruz@ee.ufcg.edu.br>
2021-11-24 17:46:18 -0300

    anar - Trazendo imagem da Home para o carrinho

--- backend-hortifruti/src/main/java/com/br/backendhortifruti/model/dto/ProdutoDTO.java
+++ backend-hortifruti/src/main/java/com/br/backendhortifruti/model/dto/ProdutoDTO.java
@@ -22,8 +22,6 @@ public class ProdutoDTO {
     
     private boolean status;
 
-    private String imagem;
-
     public ProdutoDTO(Produto produto) {
     	this.codigo = produto.getCodigo();
     	this.nome = produto.getNome();
@@ -31,7 +29,6 @@ public class ProdutoDTO {
         this.unidadeMedida = produto.getUnidadeMedida();
         this.valorUnitario = produto.getValorUnitario();
         this.status = produto.isStatus();
-        this.imagem = produto.getImagem();
     }
 
     public static List<ProdutoDTO> converterList(List<Produto> produtos) {
