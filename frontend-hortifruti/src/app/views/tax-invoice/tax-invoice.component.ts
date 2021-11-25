5db4bdcf3d84bafc1938ce8a762ebcd16bab4e9f
Samyra França <samy_franca@outlook.com>
2021-11-25 10:02:32 -0300

    criando componente da nota fiscal

---
+++ frontend-hortifruti/src/app/views/tax-invoice/tax-invoice.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-tax-invoice',
+  templateUrl: './tax-invoice.component.html',
+  styleUrls: ['./tax-invoice.component.css']
+})
+export class TaxInvoiceComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
